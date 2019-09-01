7c6c25744a4362c3b8554b94829a43520922334d
asmith <mrpresidentsmith@gmail.com>
2019-08-31 20:49:58 -0700

    Missles fire.

---
+++ Missile Command/Assets/Missile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 targetPos;
+    Rigidbody2D rb2d;
+    float speedModifier = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        rb2d.AddRelativeForce(targetPos*speedModifier, ForceMode2D.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTarget(Vector3 target)
+    {
+        Debug.Log("Setting Target");
+        targetPos = target;
+        this.transform.rotation = Quaternion.LookRotation(Vector3.forward, target - this.transform.position);
+    }
+
+}
---
+++ Missile Command/Assets/Silo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Silo : MonoBehaviour
+{
+
+    public GameObject missilePrefab;
+    private Vector3 launchPosition = new Vector3(0f, 0f, 0f);
+    Camera camera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Launch();
+        }
+    }
+
+    private void Launch()
+    {
+        //Get Mouse Position
+        Vector3 mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+        //Spawn a missle 
+        GameObject newMissile =  Instantiate(missilePrefab, launchPosition, Quaternion.identity);
+        //Set Target
+        newMissile.GetComponent<Missile>().SetTarget(mousePos);
+
+    }
+}
+
+/*Rotate Pointer towards mouse
+Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+*/
