a9943732c667f688f8116ae45779e893d0718bcf
asmith <mrpresidentsmith@gmail.com>
2019-09-07 15:00:48 -0700

    Missile create explosions.

--- Missile Command/Assets/Scripts/Missile.cs
+++ Missile Command/Assets/Scripts/Missile.cs
@@ -6,9 +6,12 @@ public class Missile : MonoBehaviour
 {
     [SerializeField]
     Vector3 targetPos;
+    [SerializeField]
+    GameObject explosionPrefab;
     Rigidbody2D rb2d;
     float speedModifier = 1f;
     float missileLifeSpan = 4f;
+    float explosionOffset = 0.1f;
     
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,13 @@ public class Missile : MonoBehaviour
         rb2d.AddRelativeForce(targetPos*speedModifier, ForceMode2D.Impulse);
 
         Destroy(this.gameObject, missileLifeSpan);
+
+        Debug.Log("Target Location:  " + targetPos.ToString() + " Current Location:  " + transform.position.ToString());
+    }
+
+    private void Update()
+    {
+        MissileHasReachedTarget();
     }
 
     public void SetTarget(Vector3 target)
@@ -25,6 +35,36 @@ public class Missile : MonoBehaviour
         Debug.Log("Setting Target for " + this.name);
         targetPos = target;
         this.transform.rotation = Quaternion.LookRotation(Vector3.forward, target - this.transform.position);
+    }
+
+    private void MissileHasReachedTarget()
+    {
+        float xDifference = Mathf.Abs(transform.position.x - targetPos.x);
+        float yDifference = Mathf.Abs(transform.position.y - targetPos.y);
+        //Debug.Log("xDiff " + xDifference.ToString() + " yDiff " + yDifference.ToString());
+        if(xDifference < explosionOffset && yDifference < explosionOffset)
+        {
+            Debug.Log("BOOM!!!");
+            Explode();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.GetComponent<EnemyMissile>() != null)
+        {
+            Debug.Log("missiles collided.");
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        //Instantiate the explosion
+        GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        
+        //Destroy this missile
+        Destroy(this.gameObject);
     }
 
 }
