2147138544258df57b9730e47a1912dec3ab103f
asmith <mrpresidentsmith@gmail.com>
2019-09-08 13:17:53 -0700

    Explosions scale up, and then destroy themselves.

--- Missile Command/Assets/Scripts/Explosion.cs
+++ Missile Command/Assets/Scripts/Explosion.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    
+    float lifespan = 0;
+    float sizeModifier = 1;
+    float maxSize = 3;
+    Transform transform;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lifespan += Time.deltaTime;
+        Debug.Log("Lifespan " + lifespan);
+        float modifier = lifespan * sizeModifier;
+        Debug.Log("Modifier " + modifier);
+        transform.localScale += new Vector3(modifier, modifier, 0f);
+        if(transform.localScale.x > maxSize)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
